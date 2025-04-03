53f438584ef74ab4353929c7fa38813c74b6b984
60sinn <39939@iitu.edu.kz>
2025-04-03 03:15:03 +0300

    Созданы модели для аниме и для админки. Временно изменена база данных для тестирования.

--- anime/admin.py
+++ anime/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Genre, Studio, Anime
 
-# Register your models here.
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+@admin.register(Studio)
+class StudioAdmin(admin.ModelAdmin):
+    list_display = ('name', 'country')
+
+@admin.register(Anime)
+class AnimeAdmin(admin.ModelAdmin):
+    list_display = ('title', 'release_date', 'studio')
+    search_fields = ('title',)
+    list_filter = ('studio', 'release_date')
--- anime/models.py
+++ anime/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+#жанр
+class Genre(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+#студия
+class Studio(models.Model):
+    name = models.CharField(max_length=100)
+    country = models.CharField(max_length=50, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+#аниме
+class Anime(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    genres = models.ManyToManyField(Genre, related_name='animes')
+    studio = models.ForeignKey(Studio, on_delete=models.SET_NULL, null=True, blank=True)
+    release_date = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
