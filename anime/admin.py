a5e651495bee134c73b168184f167c8b5887780e
60sinn <39939@iitu.edu.kz>
2025-04-05 21:35:50 +0300

    Добавлены модели для сезонов и эпизодов, добавлено поле ссылки в эпизодах. Добавлена миграция.

--- anime/admin.py
+++ anime/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Genre, Studio, Anime
+from .models import Genre, Studio, Anime, Season, Episode
 
 @admin.register(Genre)
 class GenreAdmin(admin.ModelAdmin):
@@ -14,3 +14,14 @@ class AnimeAdmin(admin.ModelAdmin):
     list_display = ('title', 'release_date', 'studio')
     search_fields = ('title',)
     list_filter = ('studio', 'release_date')
+
+
+@admin.register(Season)
+class SeasonAdmin(admin.ModelAdmin):
+    list_display = ('anime', 'season_number', 'title', 'release_date')
+    list_filter = ('anime', 'season_number')
+
+@admin.register(Episode)
+class EpisodeAdmin(admin.ModelAdmin):
+    list_display = ('season', 'episode_number', 'title', 'release_date', 'url')
+    list_filter = ('season', 'episode_number')
--- anime/models.py
+++ anime/models.py
@@ -25,3 +25,23 @@ class Anime(models.Model):
 
     def __str__(self):
         return self.title
+    
+class Season(models.Model):
+    anime = models.ForeignKey(Anime, on_delete=models.CASCADE, related_name='seasons')
+    title = models.CharField(max_length=200)
+    season_number = models.PositiveIntegerField()  # Номер сезона (1, 2, 3 и т.д.)
+    release_date = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.title} ({self.anime.title} - Season {self.season_number})"
+    
+class Episode(models.Model):
+    season = models.ForeignKey(Season, on_delete=models.CASCADE, related_name='episodes')
+    episode_number = models.PositiveIntegerField()  # Номер эпизода в сезоне (1, 2, 3 и т.д.)
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    release_date = models.DateField(null=True, blank=True)
+    url = models.URLField(max_length=200)
+
+    def __str__(self):
+        return f"Episode {self.episode_number} - {self.title}"
