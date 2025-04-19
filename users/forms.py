74abf9c653db4b9dc0959915db93fd2e4e536884
60sinn <39939@iitu.edu.kz>
2025-04-19 21:12:08 +0300

    💄 Редизайн base.html: заменена стандартная навигация на бургер-меню (три полоски), добавлено боковое меню с пунктами Anime / Shorts / Профиль
    🎨 Редизайн home.html: удалён центральный список аниме, страница адаптирована под будущий лендинг
    
    🐛 Фикс profile.html: исправлено поведение форм — теперь они скрываются после ошибки, отображается только toast
    
    🧠 Обновлена логика в views.py: улучшена обработка ошибок в профиле, убраны лишние поля при partial update
    
    🛠️ forms.py: валидаторы username/email больше не мешают другим полям и работают изолированно

--- users/forms.py
+++ users/forms.py
@@ -24,30 +24,47 @@ class ProfileUpdateForm(forms.ModelForm):
             'bio': 'Описание профиля'
         }
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # По умолчанию отключаем обязательность
+        self.fields['username'].required = False
+        self.fields['email'].required = False
+
+        # Делаем обязательными, если они реально присутствуют в POST
+        data = args[0] if args else {}
+        if 'username' in data:
+            self.fields['username'].required = True
+        if 'email' in data:
+            self.fields['email'].required = True
+
     def clean_username(self):
+        if 'username' not in self.cleaned_data:
+            return self.instance.username
         username = self.cleaned_data.get('username')
         if username != self.instance.username:
             validate_username_change(self.instance)
         return username
 
     def clean_email(self):
+        if 'email' not in self.cleaned_data:
+            return self.instance.email
         email = self.cleaned_data.get('email')
         if email != self.instance.email:
             validate_email_change(self.instance)
         return email
-    
+
     def save(self, commit=True):
         user = super().save(commit=False)
         now = timezone.now()
 
-        # ВАЖНО: сравниваем с self.instance._loaded_values
         original_username = self.initial.get('username')
         original_email = self.initial.get('email')
 
-        if original_username and self.cleaned_data.get('username') != original_username:
+        if 'username' in self.cleaned_data and self.cleaned_data.get('username') != original_username:
             user.last_username_change = now
 
-        if original_email and self.cleaned_data.get('email') != original_email:
+        if 'email' in self.cleaned_data and self.cleaned_data.get('email') != original_email:
             user.last_email_change = now
 
         if commit:
--- users/views.py
+++ users/views.py
@@ -1,13 +1,12 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth import login, logout
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
-
 from django.contrib.auth import get_user_model
-User = get_user_model()
-
 from django.http import Http404
 from .forms import RegisterForm, LoginForm, ProfileUpdateForm
+
+User = get_user_model()
 
 def register_view(request):
     if request.method == 'POST':
@@ -15,7 +14,7 @@ def register_view(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('home')  # потом заменим на нужный маршрут
+            return redirect('home')
     else:
         form = RegisterForm()
     return render(request, 'users/register.html', {'form': form})
@@ -35,20 +34,50 @@ def logout_view(request):
     logout(request)
     return redirect('home')
 
+
 class ProfileView(LoginRequiredMixin, TemplateView):
     template_name = 'users/profile.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['form'] = kwargs.get('form') or ProfileUpdateForm(instance=self.request.user)
+        context['active_field'] = kwargs.get('active_field', '')  # Указывает какая форма была активна
+        context['has_error'] = kwargs.get('has_error', False)
         return context
 
     def post(self, request, *args, **kwargs):
-        form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user)
+        form_type = request.POST.get('form_type')
+
+        # Частичные данные
+        field_mapping = {
+            'username': {'username': request.POST.get('username')},
+            'email': {'email': request.POST.get('email')},
+            'bio': {'bio': request.POST.get('bio')},
+            'avatar': request.POST
+        }
+
+        data = field_mapping.get(form_type, {})
+        files = request.FILES if form_type == 'avatar' else None
+
+        form = ProfileUpdateForm(data, files, instance=request.user)
+
+        # Удаляем поля, не относящиеся к этой форме
+        for field_name in list(form.fields.keys()):
+            if field_name not in data and field_name not in (files or {}):
+                form.fields.pop(field_name)
+
         if form.is_valid():
             form.save()
-            return self.render_to_response(self.get_context_data(form=ProfileUpdateForm(instance=request.user), success=True))
-        return self.render_to_response(self.get_context_data(form=form))
+            return redirect('profile')
+
+        # Если ошибка — не показывать форму, только toast и возврат к display
+        context = self.get_context_data(
+            form=ProfileUpdateForm(instance=request.user),
+            has_error=True,
+            active_field=''  # Сбрасываем активную форму
+        )
+        context['form'].errors.update(form.errors)
+        return self.render_to_response(context)
 
 
 class PublicProfileView(TemplateView):
